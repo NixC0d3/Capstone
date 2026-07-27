2c1f931a2a75f9dab9f655036ef8c7dfbc1c13a2
Melissa Grant <grantmelissa92@gmail.com>
2026-07-26 20:16:12 -0500

    Removed FRONTEND_before

--- FRONTEND_before/src/views/SavedOrganisationsView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container">
-    <h1>Saved Organisations</h1>
-    <p class="text-muted">Organisations saved by the current user will appear here.</p>
-  </div>
-</template>
--- FRONTEND_before/src/views/UserDashboardView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container">
-    <h1>User Dashboard</h1>
-    <p class="text-muted">General user dashboard for saved organisations, recommendations, volunteering, and messages.</p>
-  </div>
-</template>
